b6ffd05150642b89d285bb977dacf2136645b502
Naveen-Reddy-PS <naveen@stylishop.com>
2026-02-05 13:31:27 +0530

    PR check python

--- calculator.js
+++ calculator.js
@@ -17,7 +17,7 @@ function addNumbers(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number' || isNaN(a) || isNaN(b)) {
         throw new TypeError("Both arguments must be numbers");
     }
-    return a + b + 1;
+    return a + b + 2;
 }
  
 /**s
